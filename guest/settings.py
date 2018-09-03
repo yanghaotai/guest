45f6f257f88a49990dcd57ee52d8401f20815780
yanghaotai <yanghaotai@163.com>
2018-09-03 20:16:54 +0800

    接口1

--- guest/settings.py
+++ guest/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'guest.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'HOST':'127.0.0.1',
+        'HOST':'118.24.90.64',
         'PORT':'3306',
         'NAME':'guest',
         'USER':'root',
